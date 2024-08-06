05bc8a3aeec9036e1a6114ff215a7b0311e51ea3
DwikiChristian <133040158+DwikiChristian@users.noreply.github.com>
2024-08-07 00:12:46 +0800

    push fronmt-end

---
+++ front-end/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex justify-center items-center">
+    <img alt="Vue logo" src="./assets/logo.png">
+  </div>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 class="text-3xl font-bold text-red-900">Hello, Tailwind CSS!</h1>
+  </template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
